39391db2ae7e2d2c4477cc731f29f512c5ea391f
argus <tjdals4047@gmail.com>
2024-02-21 19:14:05 +0900

    Simplify expressions, add function for preserving topological order

--- identification.py
+++ identification.py
@@ -1,4 +1,5 @@
-from model import CausalDiagram
+from npsem.model import CD
+# from model import CD
 from probability import Probability, get_new_probability
 
 # Define exceptions that can occur.
@@ -9,6 +10,7 @@ class HedgeFound(Exception):
         self._message = message
         super().__init__(self._message + f"\n\nC-Forest 1:\n: {g1} \n\nC-Forest 2:\n: {g2}")
 
+
 class ThicketFound(Exception):
     '''Exception raised when a thicket is found.'''
 
@@ -17,247 +19,244 @@ class ThicketFound(Exception):
         super().__init__(self._message)
 
 
-def myID(y: set, x: set, P: "Probability", G: "CausalDiagram", verbose: bool = False, tab: int = 0):
+def get_prev_orders(order: list, left: set) -> list:
+    """
+    To preserve topological order in recursive calls.
+    """
+    return [element for element in order if element in left]
+
+
+def myID(Y: set, X: set, G: "CD", P: "Probability" = None, order: list = None, verbose: bool = False, tab: int = 0):
     """
     OUTPUT : Expression in Latex 
-
     Shpitser, Pearl 2006
     [Identification of Joint Interventional Distributions in Recursive Semi-Markovian Causal Models]
     """
-    order = G.causal_order()
+    Vs = G.V
+    if not order: order = G.causal_order()
+    if not P: P = Probability(var=Vs)
 
-    # breakpoint()
     # Line 1
-    if len(x)==0:
-        if verbose: print('line 1')
+    if not X:
+        if verbose: print(f"[(ID) line 1]")
+
         P_out = P.copy()
         if P_out._recursive:
-            P_out._sumset = P._sumset | (G.V - y)
+            P_out._sumset = P._sumset | (Vs - Y)
         else:
-            P_out._var = y
+            P_out._var = Y
         
         return P_out
         
     # line 2
-    anc = G.An(y)
-    if len(G.V- anc)>0:
-        if verbose: print('line 2')
-        
+    if Vs != G.An(Y):
+        if verbose: print(f"[(ID) line 2]\tVs: {Vs}  G.An(Y): {G.An(Y)}")
+
         P_out = P.copy()
         if P_out._recursive:
-            P_out._sumset = P._sumset | (G.V - anc)
+            P_out._sumset = P._sumset | (Vs - G.An(Y))
         else:
-            P_out._var = anc
-        # print(P_out.printLatex())
-        return myID(y, x.intersection(anc), P_out, G.induced(anc), verbose, tab=tab + 1)
+            P_out._var = G.An(Y)
+        
+        return myID(Y, X & G.An(Y), G[G.An(Y)], P_out, order, verbose, tab=tab + 1)
     
     # line 3
-    # breakpoint()
-    G_do_x = G.do(x)
-    anc_x = G_do_x.An(y)
-    W = G.V-x-anc_x
-    if len(W)>0:
-        if verbose: print('line 3')
-        # print(P.printLatex())
-        return myID(y, x.union(W), P, G, verbose, tab=tab + 1)
+    if W:=(Vs - X) - G.do(X).An(Y):
+        if verbose: print(f"[(ID) line 3]\tW: {W}")
+
+        return myID(Y, X | W, G, P, order, verbose, tab=tab + 1)
 
     # line 4
-    CCs = G[G.V-x].c_components
-    if len(CCs) > 1:
-        if verbose: print('line 4')
+    if len(CCs := G[Vs - X].c_components) > 1:
+        if verbose: print(f"[(ID) line 4]\tCCs: {CCs}")
+        
         probabilities = set()
         for CC in CCs:
-            probabilities.add(
-                myID(CC, G.V.difference(CC), P, G, verbose, tab=tab + 1))
-        return Probability(recursive=True, children=probabilities, sumset=G.V.difference(y.union(x)))
+            probabilities.add(myID(CC, Vs - CC, G, P, order, verbose, tab=tab + 1))
 
+        return Probability(recursive=True, children=probabilities, sumset=Vs - (Y | X))
 
     if len(CCs) == 1:
-        for CC in CCs:
-            S = CC
+        S = next(iter(CCs))
 
         # line 5
-        if len(G.c_components) == 1:
-            if verbose: print('line 5')
-            raise HedgeFound(G, G.induced(S))
+        if G.c_components == {Vs}:
+            if verbose: print(f"[(ID) line 5]\tC(G): {G.c_components}    Vs: {Vs}")
+            raise HedgeFound(G, G[S])
         
         # line 6
         if S in G.c_components:
-            if verbose: print('line 6')
+            if verbose: print(f"[(ID) line 6]\tS: {S}    C(G): {G.c_components}")
+
             probabilities = set()
             for vertex in S:
-                cond = set(order[:order.index(vertex)])
+                new_order = get_prev_orders(order, Vs)
+                cond = set(new_order[:new_order.index(vertex)])
                 P_out = get_new_probability(P, {vertex}, cond)
                 probabilities.add(P_out)
-            return Probability(recursive=True, children=probabilities, sumset=S.difference(y))
+            
+            return Probability(recursive=True, children=probabilities, sumset=S - Y)
         
         # line 7
-        for cc in G.c_components:
-            if S < cc:
-                if verbose: print('line 7')
-                S_prime = cc
+        for S_prime in G.c_components:
+            if S < S_prime:
+                if verbose: print(f"[(ID) line 7]\tS: {S}    S': {S_prime}")
+                
                 probabilities = set()
                 for vertex in S_prime:
-                    # breakpoint()
-                    order = G.causal_order()
-                    prev = set(order[:order.index(vertex)])
-                    cond = (prev & cc) | (prev - cc)
-                    P_out = get_new_probability(P, {vertex}, cond)
+                    new_order = get_prev_orders(order, Vs)
+                    prev = set(new_order[:new_order.index(vertex)])
+                    # cond = (prev & S_prime) | (prev - S_prime)        # Equals to cond = prev 
+                    P_out = get_new_probability(P, {vertex}, cond=prev)
                     probabilities.add(P_out)
-                
-                # print(Probability(recursive=True, children=probabilities).printLatex())
-                
-                return myID(y, x.intersection(S_prime), Probability(recursive=True, children=probabilities),
-                            G.induced(S_prime), verbose, tab=tab + 1)
+
+                return myID(Y, X & S_prime, G[S_prime], 
+                            Probability(recursive=True, children=probabilities, scope=S_prime), 
+                            order, verbose, tab=tab + 1)
 
 
-def mygID(Y: set, X: set, Z:set, P: "Probability", G: "CausalDiagram", verbose: bool = False, tab: int = 0):
-    # breakpoint()
-    # line 2 -> 여기도 line 7 처럼 수정해야할듯 이건 맞는 것 같기도 하고....
+def mygID(Y: set, X: set, Z:set, G: "CD", P: "Probability" = None, verbose: bool = False, tab: int = 0):
+    """
+    OUTPUT : Expression in Latex 
+    Lee, Correa, Bareinboim 2019
+    [General Identifiability with Arbitrary Surrogate Experiments]
+    """
+    Vs = G.V
+    if not P: P = Probability(var=Vs)
+
+    # line 2
     for z in Z:
-        if X == z & G.V: # X = {'X2'}, Z = {{'X1', 'X2'}, {'X2', 'X3'}}, G.V = {'X2', 'Y'}라면 모든 z에 대해 성립
-            if verbose: print('line 2')
+        if X == z & Vs:
+            if verbose: print(f"[(gID) line 2]\tX: {X}    Z∩V: {z & Vs}")
+            
             P_out = P.copy()
-            P_out._do = (z-G.V) | X
+            P_out._do = (z - Vs) | X
             if P_out._recursive:
-                P_out._sumset = P._sumset | (G.V - Y)
+                P_out._sumset = P._sumset | (Vs - Y)
             else:
                 P_out._var = Y
             
             return P_out
 
     # line 3
-    anc = G.An(Y)
-    if G.V != anc:
-        if verbose: print('line 3')
+    if Vs != G.An(Y):
+        if verbose: print(f"[(gID) line 3]\tVs: {Vs}  G.An(Y): {G.An(Y)}")
         
         P_out = P.copy()
         if P_out._recursive:
-            P_out._sumset = P._sumset | (G.V-anc)
+            P_out._sumset = P._sumset | (Vs - G.An(Y))
         else:
-            P_out._var = anc
-        return mygID(Y, X & anc, Z, P_out, G.induced(anc), verbose, tab=tab + 1)
+            P_out._var = G.An(Y)
+
+        return mygID(Y, X & G.An(Y), Z, G[G.An(Y)], P_out, verbose, tab=tab + 1)
     
     # line 4
-    G_do_x = G.do(X)
-    anc_x = G_do_x.An(Y)
-    W = (G.V-X) - anc_x
-    if len(W) > 0:
-        if verbose: print("line 4")
+    if W := (Vs - X) - G.do(X).An(Y):
+        if verbose: print(f"[(gID) line 4]\tW: {W}")
 
-        return mygID(Y, X | W, Z, P, G, verbose, tab=tab+1)
+        return mygID(Y, X | W, Z, G, P, verbose, tab=tab+1)
     
     # line 6
-    CCs = G[G.V-X].c_components
-    # print(CCs)
-    if len(CCs) > 1:
-        if verbose: print("line 6")
+    if len(CCs := G[Vs - X].c_components) > 1:
+        if verbose: print(f"[(gID) line 6]\tG(C\X): {CCs}")
 
         probabilities= set()
         for CC in CCs:
-            probabilities.add(
-                mygID(CC, G.V-CC, Z, P, G, verbose, tab=tab+1))
-        return Probability(recursive=True, children=probabilities, sumset=G.V - (Y|X))
+            probabilities.add(mygID(CC, Vs - CC, Z, G, P, verbose, tab=tab+1))
         
-    # line 7, 8
+        return Probability(recursive=True, children=probabilities, sumset=Vs - (Y | X))
+        
+    # line 7
     for z in Z:
-        if X >= z & G.V:
-            if verbose: print("line 7")
-            P_out = P.copy()
-            P_out._do = (z-G.V) | (X&z)
-            P_out._var = G.V
-            result = mysubID(Y, X-z, P_out, G.induced(G.V-(z&X)), verbose, tab=tab+1) 
-            if result:
-                return result
+        if X >= z & Vs:
+            if verbose: print(f"[(gID) line 7]\tX: {X}    Z∩V: {z & Vs}")
 
-    if verbose: print("line 8")
+            P_out = P.copy()
+            P_out._do = (z - Vs) | (X & z)
+            P_out._var = Vs       # useless?
+            result = mysubID(Y, X - z, G[Vs - (z & X)], P_out, verbose=verbose, tab=tab+1) 
+            
+            if result: return result
+
+    # line 8
+    if verbose: print("(gID) line 8")
     raise ThicketFound()   
         
 
-def mysubID(Y: set, X: set, Q: "Probability", G: "CausalDiagram", verbose: bool = False, tab: int = 0):
+def mysubID(Y: set, X: set, G: "CD", Q: "Probability", order: list = None, verbose: bool = False, tab: int = 0):
 
-    order = G.causal_order()
-
-    # 무조건 1개
-    # breakpoint()
-    S = next(iter(G.induced(G.V-X).c_components))
+    Vs = G.V
+    if not order: order = G.causal_order()
+    S = next(iter(G[Vs - X].c_components))
+    
     # line 11
-    if len(X)==0:
-        if verbose: print('(subID) line 11')
-        Q_out = Q.copy()
-        if Q_out._recursive:
-            Q_out._sumset = Q._sumset | (G.V - Y)
-        else:
-            Q_out._var = Y
-        return Q_out
-
-    # line 12
-    anc = G.An(Y)
-    if G.V != anc:
-        if verbose: print('(subID) line 12')
+    if not X:
+        if verbose: print("[(subID) line 11]")
         
         Q_out = Q.copy()
         if Q_out._recursive:
-            Q_out._sumset = Q._sumset.union(G.V - anc)
+            Q_out._sumset = Q._sumset | (Vs - Y)
         else:
-            Q_out._var = anc
-        return mysubID(Y, X & anc, Q_out, G.induced(anc), verbose, tab=tab + 1)
+            Q_out._var = Y
+
+        return Q_out
+
+    # line 12
+    if Vs != G.An(Y):
+        if verbose: print(f"[(subID) line 12]\tVs: {Vs}  G.An(Y): {G.An(Y)}")
+        
+        Q_out = Q.copy()
+        if Q_out._recursive:
+            Q_out._sumset = Q._sumset | (Vs - G.An(Y))
+        else:
+            Q_out._var = G.An(Y)
+
+        return mysubID(Y, X & G.An(Y), G[G.An(Y)], Q_out, order, verbose, tab=tab + 1)
     
     # line 13
-    CCs = G.c_components
-    if len(CCs) == 1:
-        if verbose: print("(subID) line 13")
+    if (CCs:=G.c_components) == {Vs}:
+        if verbose: print(f"[(subID) line 13]\tC(G): {G.c_components}    Vs: {Vs}")
+        
         return None
     
     # line 14
-    # breakpoint()
     if S in CCs:
-        if verbose: print('(subID) line 14')
+        if verbose: print(f"[(subID) line 14]\tS: {S}   C(G): {CCs}")
+        
         probabilities = set()
         for vertex in Y:
-            cond = set(order[:order.index(vertex)])
-            P_out = get_new_probability(Q, {vertex}, cond=cond)
-            probabilities.add(P_out)
-        return Probability(recursive=True, children=probabilities, sumset=S-Y)
+            new_order = get_prev_orders(order, Vs)
+            cond = set(new_order[:new_order.index(vertex)])
+            Q_out = get_new_probability(Q, {vertex}, cond)
+            probabilities.add(Q_out)
+        
+        return Probability(recursive=True, children=probabilities, sumset=S - Y)
     
     # line 15
-    for cc in CCs:
-        if S < cc:
-            if verbose: print('(subID) line 15')
-            S_prime = cc
+    for S_prime in CCs:
+        if S < S_prime:
+            if verbose: print(f"[(subID) line 15]\tS: {S}   S': {S_prime}")
+
             probabilities = set()
             for vertex in S_prime:
-                prev = set(order[:order.index(vertex)])
-                cond = (prev & S_prime) | (prev - S_prime)
-                P_out = get_new_probability(Q, {vertex}, cond=cond)
-                probabilities.add(P_out)
-            return mysubID(Y, X & S_prime, Probability(recursive=True, children=probabilities),
-                        G.induced(S_prime), verbose, tab=tab + 1)
+                new_order = get_prev_orders(order, Vs)
+                prev = set(new_order[:new_order.index(vertex)])
+                Q_out = get_new_probability(Q, {vertex}, cond=prev)
+                probabilities.add(Q_out)
+            
+            return mysubID(Y, X & S_prime, G[S_prime], 
+                           Probability(recursive=True, children=probabilities, scope=S_prime), 
+                           order, verbose, tab=tab + 1)
+
 
 if __name__ == "__main__":
 
-    # Figure 2
+    G = CD({'X', 'W1', 'W2', 'W3', 'W4', 'W5', 'Y'}, 
+                    [('X', 'Y'), ('W1', 'W2'), ('W2', 'X'), ('W4', 'X'), ('W3', 'W4')],
+                    [('W1', 'W3', 'U_W1W3'), ('W2', 'W3', 'U_W2W3'), ('W3', 'W5', 'U_W3W5'), ('W4', 'W5', 'U_W4W5'), ('W1', 'Y', 'U_W1Y'), ('W1', 'X', 'U_W1X')])
 
-    # G = CausalDiagram({'X', 'Z1', 'Z2', 'Z3', 'Y'}, 
-    #                     [('X', 'Z1'), ('Z1', 'Y'), ('Z2', 'X'), ('Z2', 'Z1'), ('Z2', 'Z3'), ('Z3', 'Y')],
-    #                     [('X', 'Z2', 'U_XZ2'), ('Y', 'Z2', 'U_YZ2'), ('X', 'Z3', 'U_XZ3'), ('X', 'Y', 'U_XY')])
-
-    # P = ID(y=set(['Y', 'Z1','Z2', 'Z3']), x=set(["X"]), P=Probability(var=G.V), G=G,verbose=True)
-    # print(f"ID: {P.printLatex()}")
-
-    # G = CausalDiagram({'X', 'W1', 'W2', 'W3', 'W4', 'W5', 'Y'}, 
-    #                 [('X', 'Y'), ('W1', 'W2'), ('W2', 'X'), ('W4', 'X'), ('W3', 'W4')],
-    #                 [('W1', 'W3', 'U_W1W3'), ('W2', 'W3', 'U_W2W3'), ('W3', 'W5', 'U_W3W5'), ('W4', 'W5', 'U_W4W5'), ('W1', 'Y', 'U_W1Y'), ('W1', 'X', 'U_W1X')])
-
-    # P = ID(y=set(['Y']), x=set(["X"]), P=Probability(var=G.V), G=G,verbose=True)
-    # # P1 = gID(Y=set(['Y']), X=set(["X"]), Z=frozenset([frozenset()]), P=Probability(var=G.V), G=G)
-    # print(f"ID: {P.printLatex()}")
-
-    G = CausalDiagram({'A', 'X', 'B', 'C', 'Y'}, 
-                    [('A', 'X'), ('A', 'B'), ('X', 'B'), ('B', 'C'), ('C', 'Y')],
-                    [('X', 'Y', 'U_XY')])
-
-    P = mygID(Y=set(['Y']), X=set(["X"]), Z=frozenset([frozenset(), frozenset('B')]), P=Probability(var=G.V), G=G)
+    P = myID(Y={'Y'}, X={'X'}, G=G, verbose=True)
     print(f"ID: {P.printLatex()}")
-
     
+    P1 = mygID(Y={'Y'}, X={'X'}, Z=[set()], G=G, verbose=True)
+    print(f"gID: {P1.printLatex()}")
